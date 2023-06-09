3bfeb1f869d08565b46420f2719173741dc6459c
Nikolai <hukolaulos@gmail.com>
2023-06-09 15:26:30 +0700

    v1.006 add readme

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #! bin/bash
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install qrencode wireguard python3-venv git
-git clone https://github.com/Nikolailavr/WireguardServ.git
-cd WireguardServ
-
 rm -rf venv
 python3 -m venv venv
 source venv/bin/activate
