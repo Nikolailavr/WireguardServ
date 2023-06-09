ce55b6944419b7127912fbe28cbecf74f4e6aff8
Nikolai <hukolaulos@gmail.com>
2023-06-09 15:02:42 +0700

    v1.004 fix bags

--- misc/const.py
+++ misc/const.py
@@ -1,7 +1,7 @@
 SERVER_CONF = """
 [Interface]
-Address = 172.16.0.1/24
-ListenPort = 51830
+Address = 172.16.1.1/24
+ListenPort = 51820
 PrivateKey = {ServPrivateKey}
 SaveConfig = false
 PostUp = iptables -I FORWARD -i %i -j ACCEPT
@@ -13,24 +13,24 @@ PostDown = iptables -D FORWARD -o %i -j ACCEPT
 Table = off
 """
 
-POSTUP = "PostUp = ip route add 192.168.{num_1}.0/24 via 172.16.0.{num_2} dev %i"
-POSTDOWN = "PostDown = ip route delete 192.168.{num_1}.0/24 via 172.16.0.{num_2} dev %i"
+POSTUP = "PostUp = ip route add 192.168.{num_1}.0/24 via 172.16.1.{num_2} dev %i"
+POSTDOWN = "PostDown = ip route delete 192.168.{num_1}.0/24 via 172.16.1.{num_2} dev %i"
 
 PEER_CONF = """
 [Peer]
 PublicKey = {PeerPublicKey}
-AllowedIPs = 172.16.0.{num_2}/32,192.168.{num_1}.0/24
+AllowedIPs = 172.16.1.{num_2}/32,192.168.{num_1}.0/24
 """
 
 CLIENT_CONF = """
 [Interface]
 PrivateKey = {PeerPrivateKey}
-Address = 172.16.0.{num_2}/24
+Address = 172.16.1.{num_2}/32
 DNS = 8.8.8.8
 
 [Peer]
 PublicKey = {ServPublicKey}
-Endpoint = {ServerIP}:51830
+Endpoint = {ServerIP}:51820
 AllowedIPs = 0.0.0.0/0
 PersistentKeepalive = 20
 """
